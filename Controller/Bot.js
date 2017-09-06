98137ade4c8c71b5908996d28790913484aadc35
Aurel Roci <aurel.roci@motius.de>
2017-09-06 15:17:07 +0200

    test

--- Controller/Bot.js
+++ Controller/Bot.js
@@ -144,8 +144,11 @@ module.exports.userBot = (controller, client) => {
     controller.hears(['prepare_member'], ['direct_message'], function (bot, message) {
         Utils.checkUserPermission(bot, message.user).then((permission) => {
             let members = message.text.match(CONSTANTS.REGEXES.userIdRegex)
-            Response.addReply(message.text);
-            bot.reply(message, "Hey");
+            Response.addReply(message.text).then((res) => {
+                bot.reply(message, res);
+            }).catch((err) => {
+                bot.reply(message, err);
+            });
             // There has to be a match
             if (members == null) {
                 bot.reply(message, CONSTANTS.RESPONSES.PREPARE_MEMBER_NO_MEMBER_ENTERED);
