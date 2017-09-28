f3533e224b76000cf396411e6325f3c4cdc29989
Aurel Roci <aurel.roci@motius.de>
2017-09-28 22:08:24 +0200

    add debuglog

--- Controller/Bot.js
+++ Controller/Bot.js
@@ -419,6 +419,7 @@ module.exports.userBot = (controller, client) => {
         logger.debug("CHANNEL CHANGE", message);
         channel = channel.trim();
         Admin.getAdmin(message.user).then((res) => {
+            logger.debug("ADMIN", message);
             if (res[0]) {
                 Admin.setChannel(message.user, channel).then((res) => {
 
